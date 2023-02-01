32a8a6591b176f5b44c8c8bd2dd01827fc882cef
JordanDlmt <109548888+JordanDlmt@users.noreply.github.com>
2023-01-31 17:04:25 -0800

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -9,6 +9,7 @@ export default function Weather() {
       <div class="weather-wrapper">
         <div class="weather-app">
           <WeatherSearch />
+
           <div class="overview">
             <h1 id="city"></h1>
             <ul>
@@ -20,7 +21,11 @@ export default function Weather() {
           </div>
           <div class="row">
             <div class="col-6">
-              <img src="#" alt="clear" id="icon" />
+              <img
+                src="http://openweathermap.org/img/wn/01d@2x.png"
+                alt=""
+                width="60"
+              />
               <span id="currentTemp">83</span>
               <span class="units">
                 <a href="#" id="farenheit-link" class="active">
@@ -43,35 +48,17 @@ export default function Weather() {
               </ul>
             </div>
           </div>
-          <div class="weather-forcast" id="forecast">
-            <div class="row">
-              <div class="col-2">
-                <div class="weather-forcast-date">Tue</div>
-                <img
-                  src="http://openweathermap.org/img/wn/01d@2x.png"
-                  alt=""
-                  width="60"
-                />
-                <div class="weather-forcast-temperatures">
-                  <span class="weather-forcast-temperature-max">75°</span>
-                  <span class="weather-forcast-temperature-min">24°</span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div id="credit">
-          <small>
-            <a
-              href="https://github.com/JordanDlmt/final-weatherapp"
-              target="_blank"
-            >
-              Open-source code
-            </a>
-            by Darlene Jordan
-          </small>
         </div>
       </div>
     </div>
   );
 }
+
+<div id="credit">
+  <small>
+    <a href="https://github.com/JordanDlmt/final-weatherapp" target="_blank">
+      Open-source code
+    </a>
+    by Darlene Jordan
+  </small>
+</div>;
