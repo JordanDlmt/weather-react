2ae658dfe0211358d7f988ebbbe976eefc460d4d
JordanDlmt <109548888+JordanDlmt@users.noreply.github.com>
2023-01-31 16:29:27 -0800

    updated components

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import WeatherSearch from "./WeatherSearch";
+import "./App.css";
 
 export default function Weather() {
   return (
