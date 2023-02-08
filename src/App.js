8a6379f075305b4a20e5959b89b0630c0d2feb01
JordanDlmt <109548888+JordanDlmt@users.noreply.github.com>
2023-02-07 19:42:38 -0800

    Added styling

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-import "./App.css";
 import React from "react";
-import WeatherSearch from "./WeatherSearch";
+import Weather from "./Weather";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
+      <div className="container">
+        <header className="App-header">
           <div class="weather-wrapper">
-            <div class="weather-app">
-              Weather App
-              <WeatherSearch />
-            </div>
+            Weather App
+            <Weather />
           </div>
-        </p>
-      </header>
+        </header>{" "}
+        <footer>
+          This project was coded by Darlene Jordan and is{" "}
+          <a
+            href="https://github.com/JordanDlmt/weather-react"
+            rel="noreferrer"
+            target="_blank"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
